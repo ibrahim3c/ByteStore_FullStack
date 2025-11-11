67793ca5dce8cb7579e2d947013e12974ce4f0c6
ibrahim hany <ihany941@gmail.com>
2025-11-12 00:28:05 +0200

    Enhance address management in profile component
    Updated customer.service.ts to manage customer addresses with new methods for adding, updating, and deleting. Improved error handling and formatting for clarity.
    
    Added styles in profile.css for modals and form elements.
    
    Revamped profile.html to include an address section with a modal for address management.
    
    Updated profile.ts to handle address data, including methods for opening modals and saving or deleting addresses, ensuring proper data fetching and updates.

--- ByStore.Client/src/app/core/services/customer.service.ts
+++ ByStore.Client/src/app/core/services/customer.service.ts
@@ -1,42 +1,69 @@
-import { HttpClient } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
-import { environment } from "../../../environments/environment";
-import { catchError, Observable, throwError } from "rxjs";
-import { Customer } from "../models/customer/customer";
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Customer } from '../models/customer/customer';
+import { Address } from '../models/customer/address';
 
 @Injectable({
-  providedIn:'any'
+  providedIn: 'any',
 })
-export class CustomerService{
-  private httpClient=inject(HttpClient)
-  private readonly apiUrl=`${environment.baseUrl}/customers`
+export class CustomerService {
+  private httpClient = inject(HttpClient);
+  private readonly apiUrl = `${environment.baseUrl}/customers`;
 
-  getCustomerByUserId(userId:string):Observable<Customer>{
-    return this.httpClient.get<Customer>(`${this.apiUrl}/user/${userId}`).pipe(catchError(this.handleError))
+  getCustomerByUserId(userId: string): Observable<Customer> {
+    return this.httpClient
+      .get<Customer>(`${this.apiUrl}/user/${userId}`)
+      .pipe(catchError(this.handleError));
   }
-  updateCustomer(customerId:string,customer:Customer){
-    const updatedCustomer={
-    firstName: customer.firstName,
-    lastName: customer.lastName,
-    dateOfBirth: customer.dateOfBirth,
-    phoneNumber: customer.phoneNumber
+  updateCustomer(customerId: string, customer: Customer) {
+    const updatedCustomer = {
+      firstName: customer.firstName,
+      lastName: customer.lastName,
+      dateOfBirth: customer.dateOfBirth,
+      phoneNumber: customer.phoneNumber,
+    };
+    return this.httpClient
+      .put<Customer>(`${this.apiUrl}/${customerId}`, updatedCustomer)
+      .pipe(catchError(this.handleError));
+  }
+
+  getCustomerAddresses(customerId: string|undefined): Observable<Address[]> {
+    return this.httpClient
+      .get<Address[]>(`${this.apiUrl}/${customerId}/addresses`)
+      .pipe(catchError(this.handleError));
+  }
+  addCustomerAddress(customerId: string|undefined, address: Address) {
+    return this.httpClient
+      .post<Address[]>(`${this.apiUrl}/${customerId}/addresses`, address)
+      .pipe(catchError(this.handleError));
+  }
+  updateCustomerAddress(customerId: string |undefined, addressId: number, address: Address) {
+    return this.httpClient
+      .put<Address[]>(`${this.apiUrl}/${customerId}/addresses/${addressId}`, address)
+      .pipe(catchError(this.handleError));
+  }
+
+  deleteCustomerAddress(customerId: string|undefined, addressId: number) {
+    return this.httpClient
+      .delete<Address[]>(`${this.apiUrl}/${customerId}/addresses/${addressId}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  // Error handling
+  private handleError(error: any): Observable<never> {
+    let errorMessage = 'An unknown error occurred!';
+
+    if (error.error instanceof ErrorEvent) {
+      // Client-side error
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
     }
-    return this.httpClient.put<Customer>(`${this.apiUrl}/${customerId}`,updatedCustomer).pipe(catchError(this.handleError))
+
+    console.error(errorMessage);
+    return throwError(() => new Error(errorMessage));
   }
-
-      // Error handling
-      private handleError(error: any): Observable<never> {
-        let errorMessage = 'An unknown error occurred!';
-
-        if (error.error instanceof ErrorEvent) {
-          // Client-side error
-          errorMessage = `Error: ${error.error.message}`;
-        } else {
-          // Server-side error
-          errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-        }
-
-        console.error(errorMessage);
-        return throwError(() => new Error(errorMessage));
-      }
 }
--- ByStore.Client/src/app/features/auth/profile/profile.ts
+++ ByStore.Client/src/app/features/auth/profile/profile.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
-import { NgbDropdown, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDropdown, NgbDropdownModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Observable, switchMap } from 'rxjs';
 import { Customer } from '../../../core/models/customer/customer';
 import { CustomTransformer } from 'typescript';
@@ -9,6 +9,7 @@ import { CustomerService } from '../../../core/services/customer.service';
 import { AuthService } from '../../../core/services/auth.service';
 import { User } from '../../../core/models/auth/User';
 import { ToastrService } from 'ngx-toastr';
+import { Address } from '../../../core/models/customer/address';
 
 @Component({
   selector: 'app-profile',
@@ -23,29 +24,6 @@ export class Profile implements OnInit {
   private toastr = inject(ToastrService);
   user$?: Observable<User | null>;
 
-  addresses = [
-    {
-      id: 1,
-      street: '123 Tech Avenue',
-      city: 'San Francisco',
-      state: 'CA',
-      postalCode: '94105',
-      country: 'USA',
-      isPrimary: true,
-      addressType: 'Shipping',
-    },
-    {
-      id: 2,
-      street: '456 Innovation Street',
-      city: 'San Jose',
-      state: 'CA',
-      postalCode: '94088',
-      country: 'USA',
-      isPrimary: false,
-      addressType: 'Billing',
-    },
-  ];
-
   ngOnInit(): void {
     this.user$ = this.userService.$user;
 
@@ -54,6 +32,10 @@ export class Profile implements OnInit {
         this.customerService.getCustomerByUserId(user.userId).subscribe((customer) => {
           console.log('the customer ', customer);
           this.customer = customer;
+
+          this.customerService.getCustomerAddresses(customer.id).subscribe((adds) => {
+            this.addresses = adds;
+          });
         });
       }
     });
@@ -70,5 +52,89 @@ export class Profile implements OnInit {
     } else {
       console.error('Customer ID is missing!');
     }
+  }
+
+  // address
+  addresses: Address[] = [];
+  currentAddress: Address = this.newEmptyAddress();
+  isEditing: boolean = false; // to know if we add or edit null = add , number = edit
+
+  @ViewChild('addressModal') addressModal!: TemplateRef<any>;
+  private modalService = inject(NgbModal);
+  openAddressModal(addr?: Address, index?: number) {
+    if (addr) {
+      this.currentAddress = { ...addr };
+      this.isEditing = true;
+    } else {
+      this.currentAddress = this.newEmptyAddress();
+      this.isEditing = false;
+    }
+    this.modalService.open(this.addressModal, { backdrop: 'static', centered: true });
+  }
+
+  saveAddress(modal: any) {
+    console.log('saveAddress starts');
+    console.log('modal', modal);
+    console.log('customerId', this.customer?.id);
+    console.log('isEditing', this.isEditing);
+
+    if (!this.customer?.id) return; // safety check
+    if (this.isEditing) {
+      // edit
+      this.customerService
+        .updateCustomerAddress(this.customer?.id, this.currentAddress.id, this.currentAddress)
+        .subscribe({
+          next: (updated) => {
+            this.customerService.getCustomerAddresses(this.customer?.id).subscribe((adds) => {
+              this.addresses = adds;
+            });
+            this.toastr.success('Address updated successfully', 'Success');
+          },
+          error: (err) => this.toastr.error(err.message || 'Error updating address', 'Error'),
+        });
+    } else {
+      // add
+      this.customerService.addCustomerAddress(this.customer?.id, this.currentAddress).subscribe({
+        next: (newAddr) => {
+          this.customerService.getCustomerAddresses(this.customer?.id).subscribe((adds) => {
+            this.addresses = adds;
+          });
+          this.toastr.success('Address added successfully', 'Success');
+        },
+        error: (err) => this.toastr.error(err.message || 'Error adding address', 'Error'),
+      });
+    }
+
+    modal.close();
+  }
+
+  deleteAddress(addressId: number) {
+    if (!this.customer?.id) return;
+    console.log("addressId",addressId)
+    this.customerService
+      .deleteCustomerAddress(this.customer?.id,addressId)
+      .subscribe({
+        next: (newAddr) => {
+          this.customerService.getCustomerAddresses(this.customer?.id).subscribe((adds) => {
+            this.addresses = adds;
+          });
+          this.toastr.success('Address deleted successfully', 'Success');
+        },
+        error: (err) => this.toastr.error(err.message || 'Error adding address', 'Error'),
+      });
+  }
+
+  private newEmptyAddress(): Address {
+    return {
+      id: 0,
+      street: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      country: '',
+      isPrimary: false,
+      addressType: 'Shipping',
+      customerName: '',
+    };
   }
 }
